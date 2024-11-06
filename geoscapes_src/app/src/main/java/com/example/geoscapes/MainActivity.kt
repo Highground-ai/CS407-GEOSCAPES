76a7ddddb1457b9339145afc2f26a3548151d86c
Highground-ai <ro.1.babaria@gmail.com>
2024-11-05 23:12:03 -0600

    Made it so we have a map for our main screen

--- geoscapes_src/app/src/main/java/com/example/geoscapes/MainActivity.kt
+++ geoscapes_src/app/src/main/java/com/example/geoscapes/MainActivity.kt
@@ -1,20 +1,58 @@
 package com.example.geoscapes
 
+import android.content.Context
+import android.content.pm.PackageManager
+import android.location.Location
 import android.os.Bundle
+import android.util.Log
+import android.util.Log.d
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationServices
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
+import com.google.android.gms.maps.model.Polyline
+import com.google.android.gms.maps.model.PolylineOptions
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var mFusedLocationClient: FusedLocationProviderClient
+    private lateinit var mMap: GoogleMap
+    private lateinit var mDestinationLatlng: LatLng
+    private lateinit var CurrentLatLng: LatLng
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        mDestinationLatlng = LatLng(43.0753, -89.4034)
+        var mapFragment =
+            supportFragmentManager.findFragmentById(R.id.map_fragment) as? SupportMapFragment
+
+        mapFragment?.getMapAsync { googleMap: GoogleMap ->
+            mMap = googleMap
+            mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(mDestinationLatlng, 15f))
+        }
+
+        mFusedLocationClient = LocationServices.getFusedLocationProviderClient(this)
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if (requestCode ==1){
+            if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED){
+                //Add something to do with this permission
+            }
         }
     }
 }
