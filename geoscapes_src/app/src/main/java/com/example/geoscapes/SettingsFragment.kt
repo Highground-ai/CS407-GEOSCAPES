d468063f78101f2da041b57d41437f3e54c00124
AlexMartens <anmartens@wisc.edu>
2024-11-18 21:48:33 -0600

    Fixed some merge conflicts

--- geoscapes_src/app/src/main/java/com/example/geoscapes/SettingsFragment.kt
+++ geoscapes_src/app/src/main/java/com/example/geoscapes/SettingsFragment.kt
@@ -1,5 +1,6 @@
 package com.example.geoscapes
 
+import android.Manifest
 import android.content.Context
 import android.content.SharedPreferences
 import android.content.pm.PackageManager
@@ -13,42 +14,29 @@ import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.example.geoscapes.databinding.FragmentSettingsBinding
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [SettingsFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class SettingsFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
-
+    private var _binding: FragmentSettingsBinding? = null
+    private val binding get() = _binding!!
     private lateinit var settingsRecyclerView: RecyclerView
     private lateinit var settingsAdapter: SettingsAdapter
     private lateinit var settingToggledKV: SharedPreferences
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
     }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        val view = inflater.inflate(R.layout.fragment_settings, container, false)
-        settingsRecyclerView = view.findViewById(R.id.settings_recycler_view)
+        _binding = FragmentSettingsBinding.inflate(inflater,container,false)
+
+        settingsRecyclerView = _binding!!.settingsRecyclerView
         settingToggledKV = requireContext().getSharedPreferences(
-            getString(R.string.settingToggledKV), Context.MODE_PRIVATE)
+            getString(R.string.settingToggledKV), Context.MODE_PRIVATE
+        )
         if (settingToggledKV.getBoolean("Dark Mode", false)) {
             AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
         }
@@ -70,36 +58,57 @@ class SettingsFragment : Fragment() {
             // TODO Switch the switch back off if the user denies the permission
             when (settingItem.title) {
                 "Location" -> {
-                    if (isChecked && ContextCompat.checkSelfPermission(requireContext(),
-                            android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-                        ActivityCompat.requestPermissions(this.requireActivity(),
-                            arrayOf(android.Manifest.permission.ACCESS_FINE_LOCATION), 1)
+                    if (isChecked && ContextCompat.checkSelfPermission(
+                            requireContext(),
+                            Manifest.permission.ACCESS_FINE_LOCATION
+                        ) != PackageManager.PERMISSION_GRANTED
+                    ) {
+                        ActivityCompat.requestPermissions(
+                            this.requireActivity(),
+                            arrayOf(Manifest.permission.ACCESS_FINE_LOCATION), 1
+                        )
                     }
                 }
 
                 "Camera" -> {
-                    if (isChecked && ContextCompat.checkSelfPermission(requireContext(),
-                            android.Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
-                        ActivityCompat.requestPermissions(this.requireActivity(),
-                            arrayOf(android.Manifest.permission.CAMERA), 1)
+                    if (isChecked && ContextCompat.checkSelfPermission(
+                            requireContext(),
+                            Manifest.permission.CAMERA
+                        ) != PackageManager.PERMISSION_GRANTED
+                    ) {
+                        ActivityCompat.requestPermissions(
+                            this.requireActivity(),
+                            arrayOf(Manifest.permission.CAMERA), 1
+                        )
                     }
                 }
 
                 "Microphone" -> {
-                    if (isChecked && ContextCompat.checkSelfPermission(requireContext(),
-                            android.Manifest.permission.RECORD_AUDIO) != PackageManager.PERMISSION_GRANTED) {
-                        ActivityCompat.requestPermissions(this.requireActivity(),
-                            arrayOf(android.Manifest.permission.RECORD_AUDIO), 1)
+                    if (isChecked && ContextCompat.checkSelfPermission(
+                            requireContext(),
+                            Manifest.permission.RECORD_AUDIO
+                        ) != PackageManager.PERMISSION_GRANTED
+                    ) {
+                        ActivityCompat.requestPermissions(
+                            this.requireActivity(),
+                            arrayOf(Manifest.permission.RECORD_AUDIO), 1
+                        )
                     }
                 }
 
                 "Notifications" -> {
-                    if (isChecked && ContextCompat.checkSelfPermission(requireContext(),
-                            android.Manifest.permission.POST_NOTIFICATIONS) != PackageManager.PERMISSION_GRANTED) {
-                        ActivityCompat.requestPermissions(this.requireActivity(),
-                            arrayOf(android.Manifest.permission.POST_NOTIFICATIONS), 1)
+                    if (isChecked && ContextCompat.checkSelfPermission(
+                            requireContext(),
+                            Manifest.permission.POST_NOTIFICATIONS
+                        ) != PackageManager.PERMISSION_GRANTED
+                    ) {
+                        ActivityCompat.requestPermissions(
+                            this.requireActivity(),
+                            arrayOf(Manifest.permission.POST_NOTIFICATIONS), 1
+                        )
                     }
                 }
+
                 "Dark Mode" -> {
                     if (isChecked) {
                         AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
@@ -117,26 +126,6 @@ class SettingsFragment : Fragment() {
             adapter = settingsAdapter
         }
 
-        return view
-    }
-
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment SettingsFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            SettingsFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+        return binding.root
     }
 }
--- geoscapes_src/app/src/main/java/com/example/geoscapes/TasksFragment.kt
+++ geoscapes_src/app/src/main/java/com/example/geoscapes/TasksFragment.kt
@@ -7,26 +7,16 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.example.geoscapes.databinding.FragmentTasksBinding
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.Job
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [TasksFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class TasksFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
+    private var _binding: FragmentTasksBinding? = null
+    private val binding get() = _binding!!
     private var job : Job? = null
 
     private lateinit var incompleteRecyclerView: RecyclerView
@@ -36,10 +26,6 @@ class TasksFragment : Fragment() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
         taskDB = TaskDatabase.getDatabase(requireContext())
     }
 
@@ -47,9 +33,10 @@ class TasksFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        val view = inflater.inflate(R.layout.fragment_tasks, container, false)
-        completedRecyclerView = view.findViewById(R.id.completedRecyclerView)
-        incompleteRecyclerView = view.findViewById(R.id.incompleteRecyclerView)
+        _binding = FragmentTasksBinding.inflate(inflater,container,false)
+
+        completedRecyclerView = _binding!!.completedRecyclerView
+        incompleteRecyclerView = _binding!!.incompleteRecyclerView
 
         job = CoroutineScope(Dispatchers.IO).launch {
             val incompleteTasks = taskDB.taskDao().getIncompleteTasks()
@@ -71,27 +58,6 @@ class TasksFragment : Fragment() {
 
         }
 
-
-        return view
-    }
-
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment SettingsFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            SettingsFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+        return binding.root
     }
 }
