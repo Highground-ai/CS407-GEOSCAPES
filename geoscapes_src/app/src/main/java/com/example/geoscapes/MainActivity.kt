d521a79cf06b60fa84b965df7edaafc3c5d96a4c
AlexMartens <anmartens@wisc.edu>
2024-11-22 13:11:11 -0600

    Test Tasks added

--- geoscapes_src/app/src/main/java/com/example/geoscapes/MainActivity.kt
+++ geoscapes_src/app/src/main/java/com/example/geoscapes/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.example.geoscapes
 
 import android.content.Context
+import android.content.SharedPreferences
 import android.content.pm.PackageManager
 import android.location.Location
 import android.os.Bundle
@@ -8,6 +9,7 @@ import android.util.Log
 import android.util.Log.d
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatDelegate
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
 import androidx.core.view.ViewCompat
@@ -24,6 +26,10 @@ import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.MarkerOptions
 import com.google.android.gms.maps.model.Polyline
 import com.google.android.gms.maps.model.PolylineOptions
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.launch
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
@@ -31,6 +37,9 @@ class MainActivity : AppCompatActivity() {
 //    private lateinit var mMap: GoogleMap
 //    private lateinit var mDestinationLatlng: LatLng
 //    private lateinit var CurrentLatLng: LatLng
+    private lateinit var taskDB: TaskDatabase
+    private var job : Job? = null
+    private lateinit var settingToggledKV: SharedPreferences
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -41,6 +50,61 @@ class MainActivity : AppCompatActivity() {
         val navController = navHost.navController
 
         binding.bottomNavigationView.setupWithNavController(navController)
+
+        settingToggledKV = this.getSharedPreferences(
+            getString(R.string.settingToggledKV), Context.MODE_PRIVATE
+        )
+        if (settingToggledKV.getBoolean("Dark Mode", false)) {
+            AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
+        } else {
+            AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+        }
+
+        taskDB = TaskDatabase.getDatabase(this)
+
+        job = CoroutineScope(Dispatchers.IO).launch {
+            for (i in 1..20) {
+                taskDB.deleteDao().delete(i)
+            }
+            taskDB.taskDao().upsert(Task(taskId=1,taskName="Test", taskDescription = null, taskCompletion=0f))
+            val testTask = taskDB.taskDao().getTaskByName("Test")
+            if (testTask != null) {
+                taskDB.stepDao().upsertStep(Step(
+                    stepName="Test Step",
+                    stepDescription = null,
+                    stepCompletion = false,
+                    location = LatLng(0.0, 0.0)),
+                    testTask.taskId)
+            }
+            taskDB.taskDao().upsert(Task(taskId=2,taskName="Test2", taskDescription = null, taskCompletion=100f))
+            val testTask2 = taskDB.taskDao().getTaskByName("Test2")
+            if (testTask2 != null) {
+                taskDB.stepDao().upsertStep(Step(
+                    stepName="Test Step 2",
+                    stepDescription = null,
+                    stepCompletion = true,
+                    location = LatLng(0.0, 0.0)),
+                    testTask2.taskId)
+            }
+            taskDB.taskDao().upsert(Task(taskId=3,taskName="Test3", taskDescription = null, taskCompletion=50f))
+            val testTask3 = taskDB.taskDao().getTaskByName("Test3")
+            if (testTask3 != null) {
+                taskDB.stepDao().upsertStep(Step(
+                    stepName="Test Step 3",
+                    stepDescription = null,
+                    stepCompletion = true,
+                    location = LatLng(0.0, 0.0)),
+                    testTask3.taskId)
+            }
+            if (testTask3 != null) {
+                taskDB.stepDao().upsertStep(Step(
+                    stepName="Test Step 4",
+                    stepDescription = null,
+                    stepCompletion = false,
+                    location = LatLng(0.0, 0.0)),
+                    testTask3.taskId)
+            }
+        }
 
 //        mDestinationLatlng = LatLng(43.0753, -89.4034)
 //        var mapFragment =
