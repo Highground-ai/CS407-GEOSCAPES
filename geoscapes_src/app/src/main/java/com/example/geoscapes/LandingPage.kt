77e9e44553c6356cd69ea5e60290e3358ecd7a5b
AlexMartens <anmartens@wisc.edu>
2024-12-03 15:09:09 -0600

    Made landing page xml and added it to nav

--- geoscapes_src/app/src/main/java/com/example/geoscapes/LandingPage.kt
+++ geoscapes_src/app/src/main/java/com/example/geoscapes/LandingPage.kt
@@ -5,6 +5,8 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import androidx.navigation.fragment.findNavController
 import com.example.geoscapes.databinding.FragmentLandingPageBinding
 import com.example.geoscapes.databinding.FragmentSettingsBinding
 import com.example.geoscapes.databinding.FragmentTasksBinding
@@ -12,6 +14,7 @@ import com.example.geoscapes.databinding.FragmentTasksBinding
 class LandingPage : Fragment() {
     private var _binding: FragmentLandingPageBinding? = null
     private val binding get() = _binding!!
+    private lateinit var goButton: Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -24,6 +27,15 @@ class LandingPage : Fragment() {
         // Inflate the layout for this fragment
         _binding = FragmentLandingPageBinding.inflate(inflater,container,false)
 
+        goButton = _binding!!.goButton
+        goButton.setOnClickListener {
+            findNavController().navigate(R.id.action_landingPageFragment_to_mapsFragment)
+        }
+        // TODO: Inflate storyline text
+
+        // TODO: Get current task and display it
+
+        // TODO: If no task, display set task
 
 
         return binding.root
