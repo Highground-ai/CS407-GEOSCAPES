bdc31951fac9baac8b5e8aa1fc1da3da549cd1c7
Highground-ai <ro.1.babaria@gmail.com>
2024-12-10 18:34:10 -0600

    Updated scene to create a badger on three lure placements

--- geoscapes_src/app/src/main/java/com/example/geoscapes/ARTemplateFragment.kt
+++ geoscapes_src/app/src/main/java/com/example/geoscapes/ARTemplateFragment.kt
@@ -1,52 +1,54 @@
 package com.example.geoscapes
 
 import android.Manifest
-import android.content.Context
+import android.R.attr.fragment
 import android.content.pm.PackageManager
+import android.net.Uri
 import android.os.Bundle
+import android.view.MotionEvent
 import android.widget.Toast
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
-import com.google.ar.core.Anchor
-import com.google.ar.core.ArCoreApk
-import com.google.ar.core.Config
-import com.google.ar.core.Session
-import com.google.ar.core.TrackingState
-import com.google.ar.sceneform.AnchorNode
-import com.google.ar.sceneform.rendering.ModelRenderable
-import com.google.ar.sceneform.ux.ArFragment
-import com.google.ar.sceneform.ux.TransformableNode
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.Job
-import kotlinx.coroutines.launch
-import android.net.Uri
-import android.util.Log
-import android.view.MotionEvent
 import androidx.lifecycle.lifecycleScope
-import androidx.navigation.Navigation
+import com.google.ar.core.Anchor
 import com.google.ar.core.Frame
 import com.google.ar.core.HitResult
 import com.google.ar.core.Plane
 import com.google.ar.core.Pose
+import com.google.ar.core.TrackingState
+import com.google.ar.sceneform.AnchorNode
 import com.google.ar.sceneform.Node
 import com.google.ar.sceneform.SceneView
 import com.google.ar.sceneform.math.Vector3
+import com.google.ar.sceneform.rendering.ModelRenderable
 import com.google.ar.sceneform.rendering.Renderable
 import com.google.ar.sceneform.rendering.ViewRenderable
+import com.google.ar.sceneform.ux.ArFragment
+import com.google.ar.sceneform.ux.TransformableNode
 import com.gorisse.thomas.sceneform.scene.await
 
+
+/**
+ * ARTemplate, used to load the lure bucky activity - allows users to place 3 lures into a scene
+ * and after they do, summon bucky.
+ */
 class ARTemplateFragment : Fragment(R.layout.fragment_ar_template) {
 
+    //Initialize fragment
     private lateinit var arFragment: ArFragment
 
+    //Get the view
     private val arSceneView get() = arFragment.arSceneView
     private val scene get() = arSceneView.scene
 
-    private var model: Renderable? = null
+    //Create the models
+    private var modelLure: Renderable? = null
+    private var modelBadger: Renderable? = null
     private var modelView: ViewRenderable? = null
 
+    //Counter to track how many lures
+    private var lureCount = 0
 
     override fun onViewCreated(view: android.view.View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
@@ -69,38 +71,75 @@ class ARTemplateFragment : Fragment(R.layout.fragment_ar_template) {
     }
 
     private suspend fun loadModels() {
-        model = ModelRenderable.builder()
-            .setSource(context, Uri.parse("models/halloween.glb"))
+        modelBadger = ModelRenderable.builder()
+            .setSource(context, Uri.parse("models/badger.glb"))
             .setIsFilamentGltf(true)
             .await()
+        modelLure = ModelRenderable.builder()
+            .setSource(context,Uri.parse("models/axe.glb"))
+            .setIsFilamentGltf(true).await()
     }
 
     private fun onTapPlane(hitResult: HitResult, plane: Plane, motionEvent: MotionEvent) {
-        if (model == null) {
+        if (modelLure == null) {
             Toast.makeText(context, "Loading...", Toast.LENGTH_SHORT).show()
             return
         }
-
-        // Create the Anchor.
-        scene.addChild(AnchorNode(hitResult.createAnchor()).apply {
-            // Create the transformable model and add it to the anchor.
-            addChild(TransformableNode(arFragment.transformationSystem).apply {
-                renderable = model
-                renderableInstance.setCulling(false)
-                renderableInstance.animate(true).start()
-                // Add the View
-                addChild(Node().apply {
-                    // Define the relative position
-                    localPosition = Vector3(0.0f, 1f, 0.0f)
-                    localScale = Vector3(0.7f, 0.7f, 0.7f)
-                    renderable = modelView
+        else if (lureCount == 3){
+            Toast.makeText(context, "You're out of lures", Toast.LENGTH_SHORT).show()
+            getCameraPose()
+        }
+        else {
+            // Create the Anchor.
+            scene.addChild(AnchorNode(hitResult.createAnchor()).apply {
+                // Create the transformable model and add it to the anchor.
+                addChild(TransformableNode(arFragment.transformationSystem).apply {
+                    renderable = modelLure
+                    renderableInstance.setCulling(false)
+                    //renderableInstance.animate(true).start()
+                    // Add the View
+//                    addChild(Node().apply {
+//                        // Define the relative position
+//                        localPosition = Vector3(0.0f, 1f, 0.0f)
+//                        localScale = Vector3(0.7f, 0.7f, 0.7f)
+//                        renderable = modelView
+//                    })
                 })
             })
-        })
+            lureCount += 1
+        }
     }
 
+    private fun getCameraPose(){
+        val frame: Frame = arFragment.arSceneView.arFrame!!
+        val camera = frame.camera
+        lateinit var CameraPose : Pose
+        if (camera.trackingState == TrackingState.TRACKING) {
+            CameraPose = camera.pose
+            createBadger(CameraPose)
+        }
 
+    }
 
+    private fun createBadger(CameraPose : Pose){
+        val anchor: Anchor = arSceneView.session?.createAnchor(CameraPose)!!
+        val anchorNode = AnchorNode(anchor)
+        anchorNode.parent = scene
+        anchorNode.addChild(TransformableNode(arFragment.transformationSystem).apply {
+            renderable = modelBadger
+            renderableInstance.setCulling(false)
+            //renderableInstance.animate(true).start()
+        })
+//        //anchorNode.setRenderable(modelBadger)
+//        //anchorNode.setParent(arFragment.arSceneView.scene)
+//        scene.addChild(anchorNode).apply {
+//            anchorNode.addChild(TransformableNode(arFragment.transformationSystem).apply {
+//                renderable = modelBadger
+//                renderableInstance.setCulling(false)
+//            })
+//        }
+
+    }
 
     private fun checkPermissionsAndInitialize() {
         if (hasCameraPermission()) {
