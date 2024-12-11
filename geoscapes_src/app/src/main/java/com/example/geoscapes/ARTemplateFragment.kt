a7ed45be4a5a196815998929e421267363591339
Highground-ai <ro.1.babaria@gmail.com>
2024-12-10 21:05:33 -0600

    Updated to navigate out of activity upon clicking the badger

--- geoscapes_src/app/src/main/java/com/example/geoscapes/ARTemplateFragment.kt
+++ geoscapes_src/app/src/main/java/com/example/geoscapes/ARTemplateFragment.kt
@@ -1,7 +1,8 @@
 package com.example.geoscapes
 
 import android.Manifest
-import android.R.attr.fragment
+import android.content.Context
+import android.content.SharedPreferences
 import android.content.pm.PackageManager
 import android.net.Uri
 import android.os.Bundle
@@ -11,6 +12,7 @@ import androidx.activity.result.contract.ActivityResultContracts
 import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.lifecycleScope
+import androidx.navigation.Navigation
 import com.google.ar.core.Anchor
 import com.google.ar.core.Frame
 import com.google.ar.core.HitResult
@@ -18,8 +20,10 @@ import com.google.ar.core.Plane
 import com.google.ar.core.Pose
 import com.google.ar.core.TrackingState
 import com.google.ar.sceneform.AnchorNode
+import com.google.ar.sceneform.HitTestResult
 import com.google.ar.sceneform.Node
 import com.google.ar.sceneform.SceneView
+import com.google.ar.sceneform.math.Quaternion
 import com.google.ar.sceneform.math.Vector3
 import com.google.ar.sceneform.rendering.ModelRenderable
 import com.google.ar.sceneform.rendering.Renderable
@@ -27,6 +31,11 @@ import com.google.ar.sceneform.rendering.ViewRenderable
 import com.google.ar.sceneform.ux.ArFragment
 import com.google.ar.sceneform.ux.TransformableNode
 import com.gorisse.thomas.sceneform.scene.await
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.future.await
+import kotlinx.coroutines.launch
 
 
 /**
@@ -46,13 +55,23 @@ class ARTemplateFragment : Fragment(R.layout.fragment_ar_template) {
     private var modelLure: Renderable? = null
     private var modelBadger: Renderable? = null
     private var modelView: ViewRenderable? = null
+    private var modelViewTwo: ViewRenderable? = null
+
+    //initialize database stuff
+    private var job : Job? = null
+    private lateinit var taskDB: TaskDatabase
+    private lateinit var currentTask: SharedPreferences
 
     //Counter to track how many lures
     private var lureCount = 0
+    private var badgerCount = 0
 
     override fun onViewCreated(view: android.view.View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         checkPermissionsAndInitialize()
+        currentTask = activity?.getSharedPreferences(
+            getString(R.string.currentTaskKey), Context.MODE_PRIVATE)!!
+        taskDB = TaskDatabase.getDatabase(requireActivity())
         arFragment = (childFragmentManager.findFragmentById(R.id.ARTemplateFragment) as ArFragment)
             .apply {
                 setOnSessionConfigurationListener{ session,config ->
@@ -78,12 +97,21 @@ class ARTemplateFragment : Fragment(R.layout.fragment_ar_template) {
         modelLure = ModelRenderable.builder()
             .setSource(context,Uri.parse("models/axe.glb"))
             .setIsFilamentGltf(true).await()
+
+        modelView = ViewRenderable.builder()
+            .setView(context, R.layout.bucky_dialog)
+            .build().await()
+        modelViewTwo = ViewRenderable.builder()
+            .setView(context, R.layout.bucky_dialog_2)
+            .build().await()
     }
 
     private fun onTapPlane(hitResult: HitResult, plane: Plane, motionEvent: MotionEvent) {
         if (modelLure == null) {
             Toast.makeText(context, "Loading...", Toast.LENGTH_SHORT).show()
             return
+        }else if (badgerCount == 1){
+            Toast.makeText(context, "You must capture the beast", Toast.LENGTH_SHORT).show()
         }
         else if (lureCount == 3){
             Toast.makeText(context, "You're out of lures", Toast.LENGTH_SHORT).show()
@@ -96,14 +124,7 @@ class ARTemplateFragment : Fragment(R.layout.fragment_ar_template) {
                 addChild(TransformableNode(arFragment.transformationSystem).apply {
                     renderable = modelLure
                     renderableInstance.setCulling(false)
-                    //renderableInstance.animate(true).start()
-                    // Add the View
-//                    addChild(Node().apply {
-//                        // Define the relative position
-//                        localPosition = Vector3(0.0f, 1f, 0.0f)
-//                        localScale = Vector3(0.7f, 0.7f, 0.7f)
-//                        renderable = modelView
-//                    })
+
                 })
             })
             lureCount += 1
@@ -115,30 +136,57 @@ class ARTemplateFragment : Fragment(R.layout.fragment_ar_template) {
         val camera = frame.camera
         lateinit var CameraPose : Pose
         if (camera.trackingState == TrackingState.TRACKING) {
-            CameraPose = camera.pose
+            CameraPose = camera.displayOrientedPose
             createBadger(CameraPose)
         }
-
     }
 
+
+
     private fun createBadger(CameraPose : Pose){
-        val anchor: Anchor = arSceneView.session?.createAnchor(CameraPose)!!
+        Toast.makeText(context, "The Badger Lurks", Toast.LENGTH_SHORT).show()
+        badgerCount += 1
+        //TODO: Rotate the badger
+        var translation = CameraPose.translation
+        var rot = CameraPose.rotationQuaternion
+        translation.set(1,translation[1] - 1)
+        val anchor: Anchor = arSceneView.session?.createAnchor(Pose(translation,rot))!!
         val anchorNode = AnchorNode(anchor)
+        //Navigate away from the badger on tap
+
         anchorNode.parent = scene
         anchorNode.addChild(TransformableNode(arFragment.transformationSystem).apply {
             renderable = modelBadger
             renderableInstance.setCulling(false)
-            //renderableInstance.animate(true).start()
-        })
-//        //anchorNode.setRenderable(modelBadger)
-//        //anchorNode.setParent(arFragment.arSceneView.scene)
-//        scene.addChild(anchorNode).apply {
-//            anchorNode.addChild(TransformableNode(arFragment.transformationSystem).apply {
-//                renderable = modelBadger
-//                renderableInstance.setCulling(false)
-//            })
-//        }
 
+            setOnTapListener { hitResult: HitTestResult?, motionEvent: MotionEvent? ->
+                Toast.makeText(context, "You've Caught The Badger", Toast.LENGTH_SHORT).show()
+                //Set Task to Complete
+                job = CoroutineScope(Dispatchers.IO).launch {
+                    val arTask = taskDB.taskDao().getTaskByName(getString(R.string.fourth_task_name))
+                    arTask?.taskCompletion = 100f
+                    taskDB.taskDao().upsert(arTask!!)
+                    //Set Current Task to None
+                    currentTask.edit().putInt("taskID", -1).apply()
+                }
+                //Navigate to Landing Page
+                Navigation.findNavController(requireView())
+                    .navigate(R.id.action_ArTemplateFragment_to_landingFragment)
+            }
+
+            addChild(Node().apply {
+                // Define the relative position
+                localPosition = Vector3(0.0f, 1f, 0.0f)
+                localScale = Vector3(0.7f, 0.7f, 0.7f)
+                renderable = modelView
+            })
+            addChild(Node().apply {
+                // Define the relative position
+                localPosition = Vector3(0.0f, 1f, 0.5f)
+                localScale = Vector3(0.7f, 0.7f, 0.7f)
+                renderable = modelViewTwo
+            })
+        })
     }
 
     private fun checkPermissionsAndInitialize() {
